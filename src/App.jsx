f0bf49aecbc7258364aa223f5861ef63ef97a273
Piyush Chauhan <piyushchauhan3554@gmail.com>
2025-11-12 09:01:55 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-// import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 // import About from "./components/About/About";
 // import Skills from "./components/Skills/Skills";
 // import Experience from "./components/Experience/Experience";
@@ -13,7 +13,9 @@ const App = () => {
     <>
       <div className="bg-[#050414]">
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-        <div className="relative pt-20"></div>
+        <div className="relative pt-20">
+          <Navbar/>
+        </div>
       </div>
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,158 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
-  return <div></div>;
+  const [activeSection, setActiveSection] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skills", label: "Skills" },
+    { id: "experience", label: "Experience" },
+    { id: "work", label: "Work" },
+    { id: "education", label: "Education" },
+  ];
+
+  const handleMenuItemClick = (id) => {
+    setActiveSection(id);
+    setIsOpen(false);
+  };
+  return (
+    <nav
+      className={`fixed top-0 w-full transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] ${
+        isScrolled
+          ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md"
+          : "bg-transparent"
+      } `}
+    >
+      <div className="text-white py-5 flex justify-between items-center">
+        {/* logo */}
+        <div className="text-lg font-semibold cursor-pointer">
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white">Piyush</span>
+          <span className="text-[#8245ec]">/</span>
+          <span className="text-white">Chauhan</span>
+          <span className="text-[#8245ec]">&gt;</span>
+        </div>
+
+        {/* desktop menu items */}
+
+        <ul className="md:flex hidden space-x-8 text-gray-300">
+          {menuItems.map((item) => (
+            <li key={item.id}>
+              <button
+                onClick={() => handleMenuItemClick(item.id)}
+                className={`
+            cursor-pointer hover:text-[#8245ec] ${
+              activeSection === item.id ? "text-[#8245ec]" : ""
+            }`}
+              >
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+        {/* social media icons  */}
+        <div className="hidden md:flex space-x-4">
+          <a
+            href="https://github.com/piyushchauhan3554"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/piyush-chauhan-972108259/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaLinkedin size={24} />
+          </a>
+          <a
+            href="https://leetcode.com/u/piyushchauhan8279/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <SiLeetcode size={24} />
+          </a>
+        </div>
+
+        {/* mobile screen menu icon */}
+        <div className="md:hidden">
+          {isOpen ? (
+            <FiX
+              className="text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            />
+          ) : (
+            <FiMenu
+              className="text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(true)}
+            />
+          )}
+        </div>
+      </div>
+      {/* mobile menu items */}
+      {isOpen && (
+        <div className="absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-50 backdrop-blur-lg z-50 rounded-lg shadow-lg">
+          <ul className="flex flex-col items-center space-y-4 py-4 text-gray-300">
+            {menuItems.map((item) => (
+              <li key={item.id}>
+                <button
+                  onClick={() => handleMenuItemClick(item.id)}
+                  className={`cursor-pointer hover:text-[#8245ec] ${
+                    activeSection === item.id ? "text-[#8245ec]" : ""
+                  }`}
+                >
+                  {item.label}
+                </button>
+              </li>
+            ))}
+            <div className="flex space-x-4">
+              <a
+            href="https://github.com/piyushchauhan3554"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/piyush-chauhan-972108259/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaLinkedin size={24} />
+          </a>
+          <a
+            href="https://leetcode.com/u/piyushchauhan8279/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <SiLeetcode size={24} />
+          </a>
+            </div>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
